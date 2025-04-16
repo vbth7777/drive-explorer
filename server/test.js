43d343a366d443d8c02446101546c2bcdd6e1ec4
vbth7777 <vbth7777@gmail.com>
2025-04-16 23:06:17 +0700

    16/04/2025

--- server/test.js
+++
@@ -1,5 +0,0 @@
-const axios = require("axios");
-//post method request
-axios.post(
-  "http://localhost:9000/thumbnail?path=Iwara/LikeHugeB%20(user3207206)/10%7C%20Kafka%20%E3%83%8F%E3%83%B3%E3%82%BF%E3%83%BC%E3%81%A8%E7%8D%B2%E7%89%A9%20Sparkle%20%E5%8C%B9%E8%AB%BE%E5%BA%B7%E5%B0%BC%E5%9C%B0%E4%B8%8B%E9%99%B7%E9%98%B1%20BlackSwan%EF%BC%88%E4%B8%8B%EF%BC%89%20-%20%5BSource%5D%20Kafka%20%E3%83%8F%E3%83%B3%E3%82%BF%E3%83%BC%E3%81%A8%E7%8D%B2%E7%89%A9%20Sparkle%20%E5%8C%B9%E8%AB%BE%E5%BA%B7%E5%B0%BC%E5%9C%B0%E4%B8%8B%E9%99%B7%E9%98%B1%20BlackSwan%EF%BC%88%E4%B8%8B%EF%BC%89.mp4%20-%20CF58QOW353EOqs.mp4",
-);
